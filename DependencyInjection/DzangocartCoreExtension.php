fd21f789103732872886a98e4956a461191c7b6a
Olivier Pichon <op@united-asian.com>
2014-01-02 10:45:50 +0700

    added Dzangocart twig extension

--- DependencyInjection/DzangocartCoreExtension.php
+++ DependencyInjection/DzangocartCoreExtension.php
@@ -2,7 +2,9 @@
 
 namespace Dzangocart\Bundle\CoreBundle\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
@@ -20,5 +22,7 @@ class DzangocartCoreExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config/services'));
+        $loader->load('twig.yml');
     }
 }
---
+++ Twig/Extension/DzangocartExtension.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Dzangocart\Bundle\CoreBundle\Twig\Extension;
+
+use \Twig_Extension;
+use \Twig_Filter_Method;
+use \Twig_Function_Method;
+
+class DzangocartExtension extends Twig_Extension
+{
+    public function getName()
+    {
+        return 'dzangoacart';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFunctions()
+    {
+        return array(
+            'app_version' => new Twig_Function_Method($this, 'getVersion', array('is_safe' => array('html'))),
+        );
+    }
+
+    /**
+     * Returns the version number from bin/version.txt
+     * TODO [OP 2013-08-01] This code stinks! There's got to be a better way to implement this.
+     */
+    public function getVersion()
+    {
+        return file_get_contents(
+            dirname(__FILE__) . "/../../../../../../../../bin/version.txt"
+        );
+    }
+}
