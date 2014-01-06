147a78b68d11ae584dc47df3798091408a107b1d
jeetendra <jeetendra@crossovernepal.com>
2014-01-06 12:40:03 +0545

    user_setting table is added to schema

---
+++ Model/UserSettings.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dzangocart\Bundle\CoreBundle\Model;
+
+use Dzangocart\Bundle\CoreBundle\Model\om\BaseUserSettings;
+
+class UserSettings extends BaseUserSettings
+{
+}
---
+++ Model/UserSettingsPeer.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dzangocart\Bundle\CoreBundle\Model;
+
+use Dzangocart\Bundle\CoreBundle\Model\om\BaseUserSettingsPeer;
+
+class UserSettingsPeer extends BaseUserSettingsPeer
+{
+}
