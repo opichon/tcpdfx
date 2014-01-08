3b1b07d822d3d523143bf7acc97c84b748408f40
jeetendra <jeetendra@crossovernepal.com>
2014-01-08 14:57:59 +0545

    currency schema and class created

---
+++ Model/Currency.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dzangocart\Bundle\CoreBundle\Model;
+
+use Dzangocart\Bundle\CoreBundle\Model\om\BaseCurrency;
+
+class Currency extends BaseCurrency
+{
+}
---
+++ Model/CurrencyPeer.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dzangocart\Bundle\CoreBundle\Model;
+
+use Dzangocart\Bundle\CoreBundle\Model\om\BaseCurrencyPeer;
+
+class CurrencyPeer extends BaseCurrencyPeer
+{
+}
