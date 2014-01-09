a2efa0d7fffd3b336bf97104a70970de8d687858
jeetendra <jeetendra@crossovernepal.com>
2014-01-09 18:11:47 +0545

    gateway engine added in schema.xml

---
+++ Model/GatewayEnginePeer.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dzangocart\Bundle\CoreBundle\Model;
+
+use Dzangocart\Bundle\CoreBundle\Model\om\BaseGatewayEnginePeer;
+
+class GatewayEnginePeer extends BaseGatewayEnginePeer
+{
+}
