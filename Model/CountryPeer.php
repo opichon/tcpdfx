259e4ed6db4f9c0ea3a813c209d10068568d7d71
jeetendra <jeetendra@crossovernepal.com>
2014-01-09 11:43:28 +0545

    country schema and class created

---
+++ Model/CountryPeer.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dzangocart\Bundle\CoreBundle\Model;
+
+use Dzangocart\Bundle\CoreBundle\Model\om\BaseCountryPeer;
+
+class CountryPeer extends BaseCountryPeer
+{
+}
---
+++ Model/CountryQuery.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dzangocart\Bundle\CoreBundle\Model;
+
+use Dzangocart\Bundle\CoreBundle\Model\om\BaseCountryQuery;
+
+class CountryQuery extends BaseCountryQuery
+{
+}
