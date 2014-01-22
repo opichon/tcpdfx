caad88cc9ac28088e1ce7da5f4e62a8ca4091a65
jeetendra <jeetendra@crossovernepal.com>
2014-01-22 17:14:42 +0545

    namespace added

--- Model/Store.php
+++ Model/Store.php
@@ -2,6 +2,7 @@
 
 namespace Dzangocart\Bundle\CoreBundle\Model;
 
+use Dzangocart\Bundle\CoreBundle\Model\CategoryPeer;
 use Dzangocart\Bundle\CoreBundle\Model\om\BaseStore;
 
 class Store extends BaseStore
