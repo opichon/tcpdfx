3e0fa0f70e98000c3b4e0c3d22655fe10f8761f8
jeetendra <jeetendra@crossovernepal.com>
2014-01-31 15:37:51 +0545

    BaseController added to core-bundle

---
+++ Controller/BaseController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Dzangocart\Bundle\CoreBundle\Controller
+
+use Dzangocart\Bundle\CoreBundle\Model\StoreQuery
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class BaseController extends Controller
+{
+    protected $store;
+    
+    public function getStore()
+    {
+        //return $store;
+        return StoreQuery::create()->findPk(5);
+    }
+}
