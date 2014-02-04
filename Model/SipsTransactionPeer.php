550f190ba7a55903362d48b20ae9b2891ac6f433
jeetendra <jeetendra@crossovernepal.com>
2014-02-04 12:30:52 +0545

    schema created for sips_transaction table

---
+++ Model/SipsTransactionPeer.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dzangocart\Bundle\CoreBundle\Model;
+
+use Dzangocart\Bundle\CoreBundle\Model\om\BaseSipsTransactionPeer;
+
+class SipsTransactionPeer extends BaseSipsTransactionPeer
+{
+}
---
+++ Model/SipsTransactionQuery.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dzangocart\Bundle\CoreBundle\Model;
+
+use Dzangocart\Bundle\CoreBundle\Model\om\BaseSipsTransactionQuery;
+
+class SipsTransactionQuery extends BaseSipsTransactionQuery
+{
+}
