1a17a03050c9062bc7d33faca50e20101422047f
jeetendra <jeetendra@crossovernepal.com>
2014-02-06 16:16:06 +0545

    service container added

--- EventListener/StoreListener.php
+++ EventListener/StoreListener.php
@@ -4,13 +4,22 @@ namespace Dzangocart\Bundle\CoreBundle\EventListener;
 
 use Dzangocart\Bundle\CoreBundle\Exception\UnknownStoreException;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\ContainerAware;
 use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
-class StoreListener 
+class StoreListener extends ContainerAware
 {
+    private $containerInterface;
+    
+    public function __construct(ContainerInterface $containerInterface)
+    {
+        $this->containerInterface = $containerInterface;
+    }
+    
     public function onFilterController(FilterControllerEvent $event)
     {
         $host_parts = array();
