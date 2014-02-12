216c2edc4cb887fc46fde013156431e622287a68
jeetendra <jeetendra@crossovernepal.com>
2014-02-12 11:04:28 +0545

    gateway service schema created

---
+++ Model/GatewayServicePeer.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dzangocart\Bundle\CoreBundle\Model;
+
+use Dzangocart\Bundle\CoreBundle\Model\om\BaseGatewayServicePeer;
+
+class GatewayServicePeer extends BaseGatewayServicePeer
+{
+}
---
+++ Model/GatewayServiceQuery.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dzangocart\Bundle\CoreBundle\Model;
+
+use Dzangocart\Bundle\CoreBundle\Model\om\BaseGatewayServiceQuery;
+
+class GatewayServiceQuery extends BaseGatewayServiceQuery
+{
+}
