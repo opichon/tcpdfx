9cef32644667922d71b1e525129e1dd873c098c8
jeetendra <jeetendra@crossovernepal.com>
2014-02-19 17:03:21 +0545

    promotion schema created

---
+++ Model/Promotion/Promotion.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dzangocart\Bundle\CoreBundle\Model\Promotion;
+
+use Dzangocart\Bundle\CoreBundle\Model\Promotion\om\BasePromotion;
+
+class Promotion extends BasePromotion
+{
+}
---
+++ Model/Promotion/PromotionQuery.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dzangocart\Bundle\CoreBundle\Model\Promotion;
+
+use Dzangocart\Bundle\CoreBundle\Model\Promotion\om\BasePromotionQuery;
+
+class PromotionQuery extends BasePromotionQuery
+{
+}
