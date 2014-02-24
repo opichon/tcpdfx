f15e7940ab40830ddafacf30fca92d6f64d7888e
jeetendra <jeetendra@crossovernepal.com>
2014-02-24 17:22:59 +0545

    nemsapce added

--- Controller/PromotionController.php
+++ Controller/PromotionController.php
@@ -3,6 +3,7 @@
 namespace Dzangocart\Bundle\CoreBundle\Controller;
 
 use Dzangocart\Bundle\CoreBundle\Form\Type\PromotionEditType;
+use Dzangocart\Bundle\CoreBundle\Model\Promotion\PromotionI18nQuery;
 use Dzangocart\Bundle\CoreBundle\Model\Promotion\PromotionQuery;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
