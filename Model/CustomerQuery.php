fbcf2e7b7ca425d2533db56b024fc57f90cca0a1
unknown <prajwol>
2014-02-25 11:09:42 -0800

    customer page

--- Model/CustomerQuery.php
+++ Model/CustomerQuery.php
@@ -13,48 +13,66 @@ class CustomerQuery extends BaseCustomerQuery
      public function dataTablesSort(ParameterBag $params, array $columns = array())
      {
         $control = 0;
+        
         for ($i = 0; $i < $params->get('iSortingCols'); $i++) {
+           
             $index = $params->get('iSortCol_' . $i);
+            
             if (array_key_exists($index, $columns)) {
+                
                 $sort_columns = $columns[$index];
+                
                 $dir = 'desc' == strtolower($params->get('sSortDir_' . $i)) ? Criteria::DESC : Criteria::ASC;
+                
                 if (!is_array($sort_columns)) {
                     $sort_columns = array($sort_columns);
                 }
+                
                 foreach ($sort_columns as $column) {
                     $this->orderBy($column, $dir);
                 }
+                
                 $control++;
             }
         }
  
         return $control ? $this : $this->defaultSort();
     }
+     protected function defaultSort()
+    {
+        return $this->orderBy('customer.id');
+    }
 
     public function dataTablesSearch($search, array $columns = array())
     {
-        $search = trim($search);
-
-        if (empty($search)) { return $this; }
+        if (empty($filters)) {
+            return $this;
+        }
 
         $conditions = array();
 
-        foreach ($columns as $i => $column) {
+        foreach ($columns as $name => $condition) {
+            if (!array_key_exists($name, $filters)) {
+                continue;
+            }
+
+            $value = trim($filters[$name]);
+
+            if (empty($value) && !is_numeric($value)) {
+                continue;
+            }
+
             $this->condition(
-                'search_' . $i,
-                sprintf('%s LIKE ?', $column),
-                sprintf('%%%s%%', $search)
+                'search_' . $name,
+                sprintf($condition, $value)
             );
 
-            $conditions [] = 'search_' . $i;
+            $conditions[] = 'search_' . $name;
         }
 
-        return $this->where($conditions, 'or');
-    }
-
-    protected function defaultSort()
-    {
-        return $this->orderBy('customer.id');
+        if (!empty($conditions)) {
+            return $this->where($conditions, 'and');
+        }
     }
 }
 
