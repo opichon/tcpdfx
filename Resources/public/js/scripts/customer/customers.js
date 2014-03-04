b8ad9aa30d02d2fdbd9d305fb10c4724b09a6ec9
unknown <prajwol>
2014-03-04 17:52:50 +0545

    customer js modified

--- Resources/public/js/scripts/customer/customers.js
+++ Resources/public/js/scripts/customer/customers.js
@@ -48,11 +48,6 @@
 
 	$.fn.customers.defaults = {
 		dataTables: {
-			aoColumnDefs: [
-				{ bSortable: false, aTargets: [ 0, 7 ] }, // First column and last column ("actions") are not sortable
-				{ bVisible: false, aTargets: [ 0 ] }, // Hide the first column
-				{ sClass: "actions", aTargets: [ 7 ] } // Set special "actions" class on cells in the last column
-			],
 			asStripeClasses: [],
 			bAutoWidth: false,
 			bPaginate: true,
