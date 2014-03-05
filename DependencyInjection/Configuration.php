51ec07614f3b174b154a447ff3f9f8ca21a02edc
Olivier Pichon <op@united-asian.com>
2014-03-05 07:16:21 +0700

    initial import

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Dzangocart\Bundle\POBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('dzangocart_po');
+
+        return $treeBuilder;
+    }
+}
---
+++ DzangocartPOBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dzangocart\Bundle\POBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DzangocartPOBundle extends Bundle
+{
+}
