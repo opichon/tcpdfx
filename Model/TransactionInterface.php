9045b88e3e7f8c191409bf25cb286de47d8849a9
Olivier Pichon <op@united-asian.com>
2014-03-07 12:16:39 +0700

    fixed TransactionInterface documentation

--- Model/TransactionInterface.php
+++ Model/TransactionInterface.php
@@ -1,35 +1,33 @@
 <?php
- 
+
 namespace Dzangocart\Bundle\CoreBundle\Model;
 
-
-interface TransactionInterface 
+interface TransactionInterface
 {
-    
-    /** 
-     * return the transaction id
-     * 
-     *@return integer
+
+    /**
+     * Returns the transaction id
+     *
+     * @return integer
      */
     public function getId();
- 
+
     /**
-     * return the amount of transaction
-     * 
+     * Returns the transaction amount
+     *
      * @return float
      */
     public function getAmount();
-    
+
     /**
      * @return Payment
      */
     public function getPayment();
 
     /**
-     * return the currency used for the transaction
-     * 
-     *@return string   
+     * Return the currency used for the transaction
+     *
+     *@return string
      */
     public function getCurrency();
-    
 }
