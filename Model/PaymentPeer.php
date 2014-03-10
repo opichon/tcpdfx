b3381f4c28ae85e3395aea64687a1ad122dc21e9
jeetendra <jeetendra@crossovernepal.com>
2014-03-10 18:37:09 +0545

    modified getOmClass method

--- Model/PaymentPeer.php
+++ Model/PaymentPeer.php
@@ -25,37 +25,18 @@ class PaymentPeer extends BasePaymentPeer
     public static function getOMClass($row = 0, $colnum = 0)
     {
         try {
+            $om_class = null;
+            $class_key = $row[$colnum + 3];
 
-            $omClass = null;
-            $classKey = $row;
+            if (array_key_exists($class_key, self::$registry)) {
 
-            switch ($classKey) {
-
-                case Payment::TYPE_PP_DIRECT:
-                    $omClass = self::$registry[Payment::TYPE_PP_DIRECT];
-                    break;
-
-                case Payment::TYPE_SIPS:
-                    $omClass = self::$registry[Payment::TYPE_SIPS];
-                    break;
-
-                case Payment::TYPE_PO:
-                    $omClass = self::$registry[Payment::TYPE_PO];
-                    break;
-
-                case Payment::TYPE_PP_EXPRESS:
-                    $omClass = self::$registry[Payment::TYPE_PP_EXPRESS];
-                    break;
-
-                default:
-                    $omClass = PaymentPeer::OM_CLASS;
-
-            } // switch
+                $om_class = self::$registry[$class_key];
+            }
 
         } catch (Exception $e) {
             throw new PropelException('Unable to get OM class.', $e);
         }
 
-        return $omClass;
+        return $om_class;
     }
 }
