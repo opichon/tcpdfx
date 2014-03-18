bddea1817d6b3aba333dc8b43139f18fe74f2568
jeetendra <jeetendra@crossovernepal.com>
2014-03-18 15:53:38 +0545

    created StoreOAuthSettings schema and table

---
+++ Model/StoreOAuthSettingsPeer.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dzangocart\Bundle\CoreBundle\Model;
+
+use Dzangocart\Bundle\CoreBundle\Model\om\BaseStoreOAuthSettingsPeer;
+
+class StoreOAuthSettingsPeer extends BaseStoreOAuthSettingsPeer
+{
+}
