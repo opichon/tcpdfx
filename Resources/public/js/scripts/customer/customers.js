0d1235ba3dead87821f4c4ecccc6f705150270d3
Olivier Pichon <op@united-asian.com>
2014-03-27 09:38:01 +0700

    restyled customers page

--- Resources/public/js/scripts/customer/customers.js
+++ Resources/public/js/scripts/customer/customers.js
@@ -49,9 +49,10 @@
 	$.fn.customers.defaults = {
 		dataTables: {
                         aoColumnDefs: [
-				{ bSortable: false, aTargets: [ 0, 7 ] }, // First column and last column ("actions") are not sortable
-				{ bVisible: false, aTargets: [ 0 ] }, // Hide the first column
-				{ sClass: "actions", aTargets: [ 7 ] } // Set special "actions" class on cells in the last column
+				{ bSortable: false, aTargets: [ 0, 7 ] },
+				{ bVisible: false, aTargets: [ 0 ] },
+				{ sClass: "number", aTargets: [ 6 ] },
+				{ sClass: "actions", aTargets: [ 7 ] }
 			],
 			asStripeClasses: [],
 			bAutoWidth: false,
