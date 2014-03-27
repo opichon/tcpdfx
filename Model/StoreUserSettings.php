f1644afd376654a2e5aad84715994f6cc4d8e3ee
Olivier Pichon <op@united-asian.com>
2014-03-27 09:08:50 +0700

    added user handling constants

--- Model/StoreUserSettings.php
+++ Model/StoreUserSettings.php
@@ -6,4 +6,7 @@ use Dzangocart\Bundle\CoreBundle\Model\om\BaseStoreUserSettings;
 
 class StoreUserSettings extends BaseStoreUserSettings
 {
+    const USER_HANDLING_NONE        = 0;
+    const USER_HANDLING_VIA_STORE   = 1;
+    const USER_HANDLING_VIA_SITE    = 2;
 }
