3a726fe9bc05d34f491a3b461dd80c4aecd05ce8
jeetendra <jeetendra@crossovernepal.com>
2014-05-12 11:23:02 +0545

    corrected formatting

--- Payment/PaymentClassRegistry.php
+++ Payment/PaymentClassRegistry.php
@@ -17,41 +17,41 @@ class PaymentClassRegistry
     }
 
     /**
-	 * Singleton pattern.
-	 *
-	 * @return PaymentClassRegistry
-	 */
-	public static function getInstance()
-	{
+     * Singleton pattern.
+     *
+     * @return PaymentClassRegistry
+     */
+    public static function getInstance()
+    {
         if (!is_object(self::$instance)) {
             self::$instance = new PaymentClassRegistry();
         }
 
         return self::$instance;
-	}
+    }
 
-	/**
-	 * Registers the class name from the payment definition against its class_key
-	 *
-	 * @throws Dzangocart\Bundle\CoreBundle\Error\Payment\DuplicateClassKeyException
-	 * 			if the class key is already defined
-	 * @throws Dzangocart\Bundle\CoreBundle\Error\Payment\InvalidClassExpceiton
-	 *			if the class does not implement the Dzangocart\Bundle\CoreBundle\Payment\Payment
-	 * 			interface
-	 */
-	public function register(PaymentDefinition $payment_definition)
-	{
+    /**
+     * Registers the class name from the payment definition against its class_key
+     *
+     * @throws Dzangocart\Bundle\CoreBundle\Error\Payment\DuplicateClassKeyException
+     *             if the class key is already defined
+     * @throws Dzangocart\Bundle\CoreBundle\Error\Payment\InvalidClassExpceiton
+     *            if the class does not implement the Dzangocart\Bundle\CoreBundle\Payment\Payment
+     *             interface
+     */
+    public function register(PaymentDefinition $payment_definition)
+    {
 
-	}
+    }
 
-	/**
-	 * Returns a payment class name based ont he supplied class key
-	 *
-	 * @throws Dzangocart\Bundle\CoreBundle\Error\Payment\UnknownClassKeyException if
-	 * 			no class is registered under the supplied class key,
-	 */
-	public function getPaymentClass($class_key)
-	{
+    /**
+     * Returns a payment class name based ont he supplied class key
+     *
+     * @throws Dzangocart\Bundle\CoreBundle\Error\Payment\UnknownClassKeyException if
+     *             no class is registered under the supplied class key,
+     */
+    public function getPaymentClass($class_key)
+    {
 
-	}
+    }
 }
