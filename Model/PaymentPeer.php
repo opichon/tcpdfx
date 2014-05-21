39dee09f3e25443c6be8b2251944acb0f7fd7e89
jeetendra <jeetendra@crossovernepal.com>
2014-05-21 12:13:08 +0545

    corrected formating

--- Model/PaymentPeer.php
+++ Model/PaymentPeer.php
@@ -6,14 +6,13 @@ use Dzangocart\Bundle\CoreBundle\Model\om\BasePaymentPeer;
 use Dzangocart\Bundle\CoreBundle\Payment\PaymentClassRegistry;
 class PaymentPeer extends BasePaymentPeer
 {
-
     public static function getOMClass($row = 0, $colnum = 0)
     {
         $om_class = null;
         $class_key = $row[$colnum + 3];
 
         $om_class = PaymentClassRegistry::getInstance()
-           ->getPaymentClass($class_key);
+            ->getPaymentClass($class_key);
 
         return $om_class;
     }
