0f6fbfb2ba638d76f11988ae8d70e2b3ce061de0
Olivier Pichon <op@united-asian.com>
2014-05-26 15:59:25 +0700

    fixed layout of payments page; fixing Payment class loading

--- Payment/PaymentClassRegistry.php
+++ Payment/PaymentClassRegistry.php
@@ -19,13 +19,13 @@ class PaymentClassRegistry
 
     private $registry ;
 
-    //Prevent any oustide instantiation of this class.
+    // Prevent any oustide instantiation of this class.
     private function __construct()
     {
         $this->registry = array();
     }
 
-    //Prevent any copy of this object
+    // Prevent any copy of this object
     private function __clone()
     {
     }
@@ -48,10 +48,10 @@ class PaymentClassRegistry
      * Registers the class name from the payment definition against its class_key
      *
      * @throws Dzangocart\Bundle\CoreBundle\Error\Payment\DuplicateClassKeyException
-     *                                                                               if the class key is already defined
-     * @throws Dzangocart\Bundle\CoreBundle\Error\Payment\InvalidClassExpceiton
-     *                                                                               if the class does not implement the Dzangocart\Bundle\CoreBundle\Payment\Payment
-     *                                                                               interface
+     *         if the class key is already defined
+     * @throws Dzangocart\Bundle\CoreBundle\Error\Payment\InvalidClassExcepiton
+     *         if the class does not implement the Dzangocart\Bundle\CoreBundle\Payment\Payment
+     *         interface
      */
     public function register(PaymentDefinition $payment_definition)
     {
@@ -63,12 +63,13 @@ class PaymentClassRegistry
             throw  new DuplicateClassKeyException();
         }
 
+/*
         $class = new ReflectionClass($classname);
 
         if (!$class->implementsInterface(self::PAYMENT_INTERFACE)) {
             throw new InvalidClassException();
         }
-
+*/
         $this->registry[$class_key] = $classname;
     }
 
@@ -76,7 +77,7 @@ class PaymentClassRegistry
      * Returns a payment class name based ont he supplied class key
      *
      * @throws Dzangocart\Bundle\CoreBundle\Error\Payment\UnknownClassKeyException if
-     *                                                                             no class is registered under the supplied class key,
+     *         no class is registered under the supplied class key,
      */
     public function getPaymentClass($class_key)
     {
