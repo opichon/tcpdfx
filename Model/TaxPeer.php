144b6d0c220b484f9e8ba916e104aacf7e827f92
Pratik Raj Sharma <pratik@crossovernepal.com>
2014-06-05 17:27:35 +0545

    updated schema and model classes

--- Model/TaxPeer.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Dzangocart\Bundle\CoreBundle\Model;
-
-use Dzangocart\Bundle\CoreBundle\Model\om\BaseTaxPeer;
-
-class TaxPeer extends BaseTaxPeer
-{
-}
---
+++ Model/TaxRatePeer.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dzangocart\Bundle\CoreBundle\Model;
+
+use Dzangocart\Bundle\CoreBundle\Model\om\BaseTaxRatePeer;
+
+class TaxRatePeer extends BaseTaxRatePeer
+{
+}
