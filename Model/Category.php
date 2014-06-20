a7136900fb2bee632125abed68ee201d48edf674
Pratik Raj Sharma <pratik@crossovernepal.com>
2014-06-20 13:20:47 +0545

    added phpname suffix to pcode

--- Model/Category.php
+++ Model/Category.php
@@ -6,4 +6,21 @@ use Dzangocart\Bundle\CoreBundle\Model\om\BaseCategory;
 
 class Category extends BaseCategory
 {
+    public function getPcode()
+    {
+        return $this->pcode;
+    }
+
+    public function setPcode($v)
+    {
+        if ($v !== null) {
+            $v = (string) $v;
+        }
+
+        if ($this->pcode !== $v) {
+            $this->pcode = $v;
+            $this->modifiedColumns[] = CategoryPeer::PCODE;
+        }
+        return $this;
+    }
 }
