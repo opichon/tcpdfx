5dab02a61f8553ded513fc82dc7b9e5412bd4801
Jeetendra Pathak <jeetendra@crossovernepal.com>
2014-07-10 12:25:15 +0545

    refactored PaymentQuery

--- Model/PaymentQuery.php
+++ Model/PaymentQuery.php
@@ -2,48 +2,11 @@
 
 namespace Dzangocart\Bundle\CoreBundle\Model;
 
-use \Criteria;
-
 use Dzangocart\Bundle\CoreBundle\Model\om\BasePaymentQuery;
 
 class PaymentQuery extends BasePaymentQuery
 {
-   public function datatablesSort(array $order = array(), array $columns = array())
-    {
-        $control = 0;
-
-        foreach ($order as $setting) {
-
-            $index = $setting['column'];
-
-            if (array_key_exists($index, $columns)) {
-                $sort_columns = $columns[$index];
-
-                $dir = $setting['dir'] == 'asc'
-                    ? Criteria::ASC
-                    : Criteria::DESC;
-
-                if (!is_array($sort_columns)) {
-                    $sort_columns = array($sort_columns);
-                }
-
-                foreach ($sort_columns as $column) {
-                    $this->orderBy($column, $dir);
-                }
-
-                $control++;
-            }
-        }
-
-        return $control ? $this : $this->defaultSort();
-    }
-
-    protected function defaultSort()
-    {
-        return $this->orderBy('payment.orderId');
-    }
-
-    public function dataTablesSearch(array $filters = null, array $columns = array())
+    public function filter(array $filters = null, array $columns = array())
     {
         if (empty($filters)) {
             return $this;
@@ -73,5 +36,32 @@ class PaymentQuery extends BasePaymentQuery
         if (!empty($conditions)) {
             return $this->where($conditions, 'and');
         }
+    }
+
+    /**
+     * Adds sorting to the query. The sort order is provided by the $order argument
+     * in the form of a 2-dimensional array. Each array element is an array in the form of
+     * [field, direction], where:
+     * - field is a column-name in a format understandable by this Propel query
+     * - direction is either Criteria::ASC or Criteria::DESC
+     *
+     * @param $order Array a 2-dimensional array in the form of [[field1, direction1],[field2, direction2],...]
+     *
+     * @Return ItemQuery this query
+     */
+    public function sort(array $order = array())
+    {
+        /*
+         * TODO [JP 2014-07-10] There is no validation here. Is this acceptable?
+         * In some ways, yes, because this methods is called internally (not via a public API),
+         * and as long as the calling logic does the validation.
+         */
+        foreach ($order as $setting) {
+            $column = $setting[0];
+            $direction = $setting[1];
+            $this->orderBy($column, $direction);
+        }
+
+        return $this;
     }
 }
