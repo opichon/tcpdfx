95fc4e3b7dcb4e57a84fdf7c8d18ef8b5a2707e6
Olivier Pichon <op@united-asian.com>
2014-07-12 19:36:14 +0700

    Fixed config

--- config/tcpdf_config.php
+++ config/tcpdf_config.php
@@ -2,161 +2,225 @@
 /**
  * Stop the original tcpdf config file loading
  */
-define ('K_TCPDF_EXTERNAL_CONFIG', true);
+if (!defined('K_TCPDF_EXTERNAL_CONFIG')) {
+	define ('K_TCPDF_EXTERNAL_CONFIG', true);
+}
 
 /**
  * path for PDF fonts
  * use K_PATH_MAIN.'fonts/old/' for old non-UTF8 fonts
  */
-define ('K_PATH_FONTS', dirname(__FILE__).'/../data/fonts/');
+if (!defined('K_PATH_FONTS')) {
+//	define ('K_PATH_FONTS', dirname(__FILE__).'/../data/fonts/');
+}
 
 /**
  * cache directory for temporary files (full path)
  */
-define ('K_PATH_CACHE', K_PATH_MAIN.'cache/');
+if (!defined('K_PATH_CACHE')) {
+//	define ('K_PATH_CACHE', dirname(__FILE__) . 'cache/');
+}
 
 /**
- *images directory
+ * images directory
  */
-define ('K_PATH_IMAGES', K_PATH_MAIN.'images/');
+if (!defined('K_PATH_IMAGES')) {
+//	define ('K_PATH_IMAGES', K_PATH_MAIN.'images/');
+}
 
 /**
  * blank image
  */
-define ('K_BLANK_IMAGE', K_PATH_IMAGES.'_blank.png');
+if (!defined('K_BLANK_IMAGE')) {
+//	define ('K_BLANK_IMAGE', K_PATH_IMAGES.'_blank.png');
+}
 
 /**
  * page format
  */
-define ('PDF_PAGE_FORMAT', 'A4');
+if (!defined('PDF_PAGE_FORMAT')) {
+	define ('PDF_PAGE_FORMAT', 'A4');
+}
 
 /**
  * page orientation (P=portrait, L=landscape)
  */
-define ('PDF_PAGE_ORIENTATION', 'P');
+if (!defined('PDF_PAGE_ORIENTATION')) {
+	define ('PDF_PAGE_ORIENTATION', 'P');
+}
 
 /**
  * document creator
  */
-define ('PDF_CREATOR', 'TCPDF');
+if (!defined('PDF_CREATOR')) {
+	define ('PDF_CREATOR', 'TCPDF');
+}
 
 /**
  * document author
  */
-define ('PDF_AUTHOR', 'TCPDF');
+if (!defined('PDF_AUTHOR')) {
+	define ('PDF_AUTHOR', 'TCPDF');
+}
 
 /**
  * header title
  */
-define ('PDF_HEADER_TITLE', 'TCPDF Example');
+if (!defined('PDF_HEADER_TITLE')) {
+	define ('PDF_HEADER_TITLE', 'TCPDF Example');
+}
 
 /**
  * header description string
  */
-define ('PDF_HEADER_STRING', "by Nicola Asuni - Tecnick.com\nwww.tcpdf.org");
+if (!defined('PDF_HEADER_STRING')) {
+	define ('PDF_HEADER_STRING', "by Nicola Asuni - Tecnick.com\nwww.tcpdf.org");
+}
 
 /**
  * image logo
  */
-define ('PDF_HEADER_LOGO', 'tcpdf_logo.jpg');
+if (!defined('PDF_HEADER_LOGO')) {
+	define ('PDF_HEADER_LOGO', 'tcpdf_logo.jpg');
+}
 
 /**
  * header logo image width [mm]
  */
-define ('PDF_HEADER_LOGO_WIDTH', 30);
+if (!defined('PDF_HEADER_LOGO_WIDTH')) {
+	define ('PDF_HEADER_LOGO_WIDTH', 30);
+}
 
 /**
  *  document unit of measure [pt=point, mm=millimeter, cm=centimeter, in=inch]
  */
-define ('PDF_UNIT', 'mm');
+if (!defined('PDF_HEADER_LOGO_WIDTH')) {
+	define ('PDF_UNIT', 'mm');
+}
 
 /**
  * header margin
  */
-define ('PDF_MARGIN_HEADER', 5);
+if (!defined('PDF_HEADER_LOGO_WIDTH')) {
+	define ('PDF_MARGIN_HEADER', 5);
+}
 
 /**
  * footer margin
  */
-define ('PDF_MARGIN_FOOTER', 10);
+if (!defined('PDF_HEADER_LOGO_WIDTH')) {
+	define ('PDF_MARGIN_FOOTER', 10);
+}
 
 /**
  * top margin
  */
-define ('PDF_MARGIN_TOP', 27);
+if (!defined('PDF_HEADER_LOGO_WIDTH')) {
+	define ('PDF_MARGIN_TOP', 27);
+}
 
 /**
  * bottom margin
  */
-define ('PDF_MARGIN_BOTTOM', 25);
+if (!defined('PDF_HEADER_LOGO_WIDTH')) {
+	define ('PDF_MARGIN_BOTTOM', 25);
+}
 
 /**
  * left margin
  */
-define ('PDF_MARGIN_LEFT', 15);
+if (!defined('PDF_HEADER_LOGO_WIDTH')) {
+	define ('PDF_MARGIN_LEFT', 15);
+}
 
 /**
  * right margin
  */
-define ('PDF_MARGIN_RIGHT', 15);
+if (!defined('PDF_HEADER_LOGO_WIDTH')) {
+	define ('PDF_MARGIN_RIGHT', 15);
+}
 
 /**
  * default main font name
  */
-define ('PDF_FONT_NAME_MAIN', 'helvetica');
+if (!defined('PDF_HEADER_LOGO_WIDTH')) {
+	define ('PDF_FONT_NAME_MAIN', 'helvetica');
+}
 
 /**
  * default main font size
  */
-define ('PDF_FONT_SIZE_MAIN', 10);
+if (!defined('PDF_HEADER_LOGO_WIDTH')) {
+	define ('PDF_FONT_SIZE_MAIN', 10);
+}
 
 /**
  * default data font name
  */
-define ('PDF_FONT_NAME_DATA', 'helvetica');
+if (!defined('PDF_HEADER_LOGO_WIDTH')) {
+	define ('PDF_FONT_NAME_DATA', 'helvetica');
+}
 
 /**
  * default data font size
  */
-define ('PDF_FONT_SIZE_DATA', 8);
+if (!defined('PDF_HEADER_LOGO_WIDTH')) {
+	define ('PDF_FONT_SIZE_DATA', 8);
+}
 
 /**
  * default monospaced font name
  */
-define ('PDF_FONT_MONOSPACED', 'courier');
+if (!defined('PDF_HEADER_LOGO_WIDTH')) {
+	define ('PDF_FONT_MONOSPACED', 'courier');
+}
 
 /**
  * ratio used to adjust the conversion of pixels to user units
  */
-define ('PDF_IMAGE_SCALE_RATIO', 1.25);
+if (!defined('PDF_HEADER_LOGO_WIDTH')) {
+	define ('PDF_IMAGE_SCALE_RATIO', 1.25);
+}
 
 /**
  * magnification factor for titles
  */
-define('HEAD_MAGNIFICATION', 1.1);
+if (!defined('PDF_HEADER_LOGO_WIDTH')) {
+	define('HEAD_MAGNIFICATION', 1.1);
+}
 
 /**
  * height of cell respect font height
  */
-define('K_CELL_HEIGHT_RATIO', 1.25);
+if (!defined('PDF_HEADER_LOGO_WIDTH')) {
+	define('K_CELL_HEIGHT_RATIO', 1.25);
+}
 
 /**
  * title magnification respect main font size
  */
-define('K_TITLE_MAGNIFICATION', 1.3);
+if (!defined('PDF_HEADER_LOGO_WIDTH')) {
+	define('K_TITLE_MAGNIFICATION', 1.3);
+}
 
 /**
  * reduction factor for small font
  */
-define('K_SMALL_RATIO', 2/3);
+if (!defined('PDF_HEADER_LOGO_WIDTH')) {
+	define('K_SMALL_RATIO', 2/3);
+}
 
 /**
  * set to true to enable the special procedure used to avoid the overlappind of symbols on Thai language
  */
-define('K_THAI_TOPCHARS', true);
+if (!defined('PDF_HEADER_LOGO_WIDTH')) {
+	define('K_THAI_TOPCHARS', true);
+}
 
 /**
  * if true allows to call TCPDF methods using HTML syntax
  * IMPORTANT: For security reason, disable this feature if you are printing user HTML content.
  */
-define('K_TCPDF_CALLS_IN_HTML', true);
+if (!defined('PDF_HEADER_LOGO_WIDTH')) {
+	define('K_TCPDF_CALLS_IN_HTML', true);
+}
