1ca5deb3a212902a8037191fbbbfdfb73db4348b
Olivier Pichon <op@united-asian.com>
2014-07-14 17:49:53 +0700

    Added stub store test

---
+++ Tests/Model/Store/StoreStatusTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Dzangocart\Bundle\CoreBundle\Tests\Model\Store;
+
+use PHPUnit_Framework_TestCase;
+
+use Dzangocart\Bundle\CoreBundle\Model\Store;
+
+class StoreStatusTest extends PHPUnit_Framework_TestCase
+{
+	public function testConfirmed()
+	{
+	}
+
+	public function testReady()
+	{
+
+	}
+
+	public function testActive()
+	{
+
+	}
+}
