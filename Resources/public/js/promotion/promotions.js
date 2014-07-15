4e483da68af2df7131c8cea8dac41af47397b9f9
Olivier Pichon <op@united-asian.com>
2014-07-15 23:55:36 +0700

    updated promotions page

---
+++ Resources/public/js/promotion/promotions.js
@@ -0,0 +1,58 @@
+!function( $ ) {
+	$.fn.promotions = function( method ) {
+
+		var settings,
+			table;
+
+		// Public methods
+		var methods = {
+			init: function( options ) {
+				settings = $.extend( true, {}, $.fn.promotions.defaults, options );
+
+				return this.each(function() {
+					var $this = $( this );
+
+					table = $( "table.table", this ).dataTable( $.extend( true, {}, settings.datatables, {
+						fnInitComplete: function( oSettings, json ) {
+							$( oSettings.nTable ).show();
+						},
+					} ) );
+				});
+			}
+		};
+
+		if ( methods[ method ] ) {
+			return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ) );
+		}
+		else if ( typeof method === "object" || !method ) {
+			return methods.init.apply( this, arguments );
+		}
+		else {
+			$.error( "Method " +  method + " does not exist in $.promotions." );
+		}
+	};
+
+	$.fn.promotions.defaults = {
+		datatables: {
+			aoColumnDefs: [
+				{ bSortable: false, aTargets: [ 0, 5 ] },
+				{ bVisible: false, aTargets: [ 0 ] },
+				{ sClass: "actions", aTargets: [ 5 ] }
+			],
+			asStripeClasses: [],
+			bAutoWidth: false,
+			bDestroy: true,
+			bPaginate: true,
+			bProcessing: true,
+			bServerSide: true,
+			bSortable: true,
+			oLanguage: {
+				sUrl: "/bundles/uamdatatables/lang/" + dzangocart.locale + ".txt"
+			}
+		}
+	};
+} ( window.jQuery );
+
+$( document ).ready(function() {
+	$( ".promotions" ).promotions( dzangocart.promotions );
+});
