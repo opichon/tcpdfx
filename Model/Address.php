2bb50599a9aef6fee176c10d8dbc95c197354c63
Bijesh <bijesh@crossovernepal.com>
2014-08-20 10:51:24 +0545

    modified address.php

--- Model/Address.php
+++ Model/Address.php
@@ -4,10 +4,34 @@ namespace Dzangocart\Bundle\CoreBundle\Model;
 
 use Dzangocart\Bundle\CoreBundle\Model\om\BaseAddress;
 
-class Address extends BaseAddress 
+abstract class Address extends BaseAddress 
 {
+    public function getComplement() {
+
+    }
+
+    public function getCountryId() {
+
+    }
+
+    public function getLocality() {
+
+    }
+
     public function getName() {
-	
+
+    }
+
+    public function getPostalCode() {
+
+    }
+
+    public function getRegion() {
+
+    }
+
+    public function getStreet() {
+
     }
 
 }
