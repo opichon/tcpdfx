978d2ec29ed833f869d1a53f7031896d42b4f89e
pratik <pratik@crossovernepal.com>
2014-09-10 18:21:43 +0545

    added search colummns

--- Controller/PromotionController.php
+++ Controller/PromotionController.php
@@ -96,7 +96,9 @@ class PromotionController extends BaseController
     {
         return array(
             'name' => 'PromotionI18n.name LIKE "%%%s%%"',
-            'code' => 'promotion.code LIKE "%%%s%%"'
+            'code' => 'promotion.code LIKE "%%%s%%"',
+            'date_from' => 'promotion.date_from >= "%s 00:00:00"',
+            'date_to' => 'promotion.date_to <= "%s 23:59:59"'
         );
     }
 
