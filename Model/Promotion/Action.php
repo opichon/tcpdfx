99bd329eaf6487e150284868982f753a479c14e0
Jeetendra Pathak <jeetendrapathak189@gmail.com>
2014-09-11 13:51:54 +0545

    actionInterface implemented in action and freeItemAction class

--- Model/Promotion/Action.php
+++ Model/Promotion/Action.php
@@ -6,9 +6,14 @@ use Dzangocart\Bundle\CoreBundle\Model\Cart;
 use Dzangocart\Bundle\CoreBundle\Model\Promotion\om\BaseAction;
 use Dzangocart\Bundle\CoreBundle\Model\Promotion\Promotion;
 
-class Action extends BaseAction
+class Action extends BaseAction implements ActionInterface
 {
     public function execute(Cart $cart, Promotion $promotion)
     {
     }
+
+    public function isEligible(Cart $cart)
+    {
+    }
+
 }
